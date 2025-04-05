db6ee29edd04ce16138e9fa32ceedd97f4c96775
devLavinea <lavinea2411@gmail.com>
2025-04-05 15:05:52 -0300

    bug

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import { HiX } from "react-icons/hi";
-import { SectionWrapper } from "../hoc";
+
 
 const data = {
   devburguer: {
@@ -74,4 +74,4 @@ const Project = () => {
   );
 };
 
-export default SectionWrapper(Modal, "");
+export default Project;
