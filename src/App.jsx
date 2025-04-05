2562083a03fb4fd6a493b9599f034823e11cc842
devLavinea <lavinea2411@gmail.com>
2025-04-05 06:24:20 -0300

    build

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,25 @@
 import React from "react";
 import "./index.css";
 
-import{
-Navbar,
-Hero,
-Cta,
-Services,
-Tech,
-Experience,
-Works,
-Feedbacks,
-Contact,
-Whatsapp,
-StarsCanvas
+import {
+  Navbar,
+  Hero,
+  Cta,
+  Services,
+  Tech,
+  Experience,
+  Works,
+  Feedbacks,
+  Contact,
+  Whatsapp,
+  StarsCanvas,
 } from "./components";
-
-
 
 const App = () => {
   return (
     <div className="relative flex-col items-center justify-center z-0 bg-primary overflow-x-hidden w-full">
       <div className="bg-custom bg-cover bg-no-repeat bg-center">
-        <Whatsapp />  
+        <Whatsapp />
         <Navbar />
         <Cta />
         <Hero />
@@ -33,7 +31,7 @@ const App = () => {
       <Feedbacks />
       <div className="relative z-0">
         <Contact />
-        <StarsCanvas/>
+        <StarsCanvas />
       </div>
     </div>
   );
